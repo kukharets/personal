2734b9d120eaebf2c40f4ef3c63de67c26ae5d79
Taras Kukharets <taras.kukharets@gmail.com>
2016-09-08 13:45:59 +0000

    ss

--- js/index.js
+++ js/index.js
@@ -1,33 +1,44 @@
-/*global $*/
-var click = false;
+// /*global $*/
+// // var aboutOn,skillsOn,contactsOn,resumeOn = false;
+$('#skillblock').hide();
+var b = $('#infoblock').html();
+var s = $('#skillblock').html();
 $('#steplang_1, #steplang_2').hide();
 $('ul').on("mouseenter", function () {
     $('#steplang_1, #steplang_2').show();
 })
+
+
+
+
 $('ul').on("mouseleave", function () {
     $('#steplang_1, #steplang_2').hide();
 })
 $('#menu>a').on("mouseenter", function () {
-    $(this).css("border-style", "dotted").css("border-width", "1px").css("border-color", "white");
+    if ($(this).attr("clicked") !='true') {
+    $(this).css("border-color", "white");}
 })
 $('#menu>a').on("mouseleave", function () {
-    if (click == false) {
-        $(this).css("border-style", "none");
+    if ($(this).attr("clicked") !='true') {
+        $(this).css("border-color", "transparent");
     }
 })
 $('#menu>a').on("click", function () {
-    $(this).css("border-style", "dotted").css("border-width", "1px").css("border-color", "white");
-    click = true;
+    $('#menu>a').css("border-color", "transparent").attr("clicked","false");
+    $(this).attr("clicked",'true');
+    $(this).css("border-color", "darksalmon");
+    
 })
 $('#skills').on("click", function () {
-    $('#infoblock').html("<p>My skills are avesome<p/>")
+    // $('#infoblock').html("<p>My skills are avesome<p/>")
+     $('#infoblock').html(s)
 })
 $('#contacts').on("click", function () {
-    $('#infoblock').html("<p>On earch<p/>")
+    $('#infoblock').html("<p>тут будуть контакти<p/>")
 })
 $('#resume').on("click", function () {
-    $('#infoblock').html("<p>Try to google it<p/>")
+    $('#infoblock').html("<p>Тут буде реюзме<p/>")
 })
 $('#about').on("click", function () {
-    $('#infoblock').html('<img id ="me" src="img/myphotonorm.png">')
+    $('#infoblock').html(b)
 })
