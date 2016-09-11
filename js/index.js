586e736c6ac067ca46f65eaff7fe779c86ddb63d
Taras Kukharets <taras.kukharets@gmail.com>
2016-09-11 21:26:35 +0000

    sz

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 // /*global $*/
 // // var aboutOn,skillsOn,contactsOn,resumeOn = false;
 $('#skillblock').hide();
+// $('#contactsblock').hide();
+$('.show1').hide();
+$('.show2').hide();
+$('.show3').hide();
+$('.show0').hide();
+$('.show4').hide();
 var b = $('#infoblock').html();
 var s = $('#skillblock').html();
+var c = $('#contactsblock').html();
+var r = $('#resumeIn').html();
 $('#steplang_1, #steplang_2').hide();
 $('ul').on("mouseenter", function() {
     $('#steplang_1, #steplang_2').show();
@@ -34,7 +42,62 @@ $('#menu>a').on("click", function() {
 
 $('#skills').on("click", function() {
     $('#infoblock').html(s);
-    function progressNow(bar, currentSkill) {
+    
+    progressNow($('#jsbars td'), 4);
+    progressNow($('#htmlbars td'), 3);
+    progressNow($('#cssbars td'), 2);
+    progressNow($('#javabars td'), 4);
+    progressNow($('#linuxbars td'), 3);
+    
+    showOptions();
+})
+$('#contacts').on("click", function() {
+    
+    $('#infoblock').html(c);
+    $('#infoblock').attr("text-align", "left");
+    $('#contactsblock').attr("visibility", "visible");
+})
+$('#resume').on("click", function() {
+    $('#infoblock').html(r);
+    $('#resumeIn').attr("visibility", "visible");
+})
+function showOptions(){
+    
+        setTimeout(function(){
+            console.log('set');
+            $('.show0').show();
+        },600)
+        setTimeout(function(){
+            console.log('set');
+            $('.show1').show();
+        },1200)
+        setTimeout(function(){
+            console.log('set');
+            $('.show2').show();
+        },1800)
+        setTimeout(function(){
+            console.log('set');
+            $('.show3').show();
+        },2400)
+        setTimeout(function(){
+            console.log('set');
+            $('.show4').show();
+        },3000)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+function progressNow(bar, currentSkill) {
         var time = 100;
         bar.each(function(i, e) {
             var self = this;
@@ -49,21 +112,14 @@ $('#skills').on("click", function() {
             }
         })
     }
-    progressNow($('#pbars td'), 4);
-})
-
-
-
 function blinkOne(bar) {
     for (var times = 400; times < 4000; times += 400) {
         var trigger = 2;
         setTimeout(function() {
             if (trigger % 2 != 0) {
                 $(bar).css("background-color", "white")
-                console.log("timer");
-            }
+             }
             else {
-                console.log("else");
                 $(bar).css("background-color", "black");
             }
             trigger++;
@@ -73,12 +129,8 @@ function blinkOne(bar) {
 
 
 
-$('#contacts').on("click", function() {
-    $('#infoblock').html("<p>тут будуть контакти<p/>")
-})
-$('#resume').on("click", function() {
-    $('#infoblock').html("<p>Тут буде реюзме<p/>")
-})
+
+
 $('#about').on("click", function() {
     $('#infoblock').html(b)
 })
